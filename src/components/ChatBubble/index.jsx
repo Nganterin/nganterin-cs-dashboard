924769a8456d92851202bd31c537c3f8bf60baa4
Rama Diaz <ramadiaz221@gmail.com>
2025-02-13 04:27:22 +0700

    UPDATE of ChatBubble (fix of long words)

--- src/components/ChatBubble/index.jsx
+++ src/components/ChatBubble/index.jsx
@@ -1,13 +1,13 @@
 export const ChatBubble = ({ data }) => {
     if (data.is_cs_chat) {
         return (
-            <div className="flex justify-end w-full">
-                <div className="flex flex-col items-end w-max max-w-[320px] leading-1.5 p-4 border-zinc-200 bg-zinc-100 rounded-b-xl rounded-s-xl dark:bg-zinc-200">
+            <div className="flex w-full justify-end">
+                <div className="flex flex-col w-max max-w-[320px] leading-1.5 p-4 border-zinc-200 bg-zinc-100 rounded-s-xl rounded-b-xl dark:bg-zinc-200">
                     <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <span className="text-sm font-semibold text-zinc-900 dark:text-black">You</span>
+                        <span className="text-sm font-semibold text-zinc-900 dark:text-black">{data.customer.name}</span>
                         <span className="text-sm font-normal text-zinc-500 dark:text-zinc-700">{data.humanized_created_at}</span>
                     </div>
-                    <p className="text-sm font-normal py-2.5 text-zinc-900 dark:text-black">{data.message}</p>
+                    <p className="text-sm font-normal break-words py-2.5 text-zinc-900 dark:text-black">{data.message}</p>
                 </div>
             </div>
         )
@@ -18,7 +18,7 @@ export const ChatBubble = ({ data }) => {
                     <span className="text-sm font-semibold text-zinc-900 dark:text-white">{data.customer.name}</span>
                     <span className="text-sm font-normal text-zinc-500 dark:text-zinc-400">{data.humanized_created_at}</span>
                 </div>
-                <p className="text-sm font-normal py-2.5 text-zinc-900 dark:text-white">{data.message}</p>
+                <p className="text-sm font-normal break-words py-2.5 text-zinc-900 dark:text-white">{data.message}</p>
             </div>
         )
     }
