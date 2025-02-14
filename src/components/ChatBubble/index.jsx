e7efe73f97904f53c5bab66168ba87cc064ce158
Rama Diaz <ramadiaz221@gmail.com>
2025-02-14 21:27:21 +0700

    UPDATE of ChatBubble

--- src/components/ChatBubble/index.jsx
+++ src/components/ChatBubble/index.jsx
@@ -4,7 +4,7 @@ export const ChatBubble = ({ data }) => {
             <div className="flex w-full justify-end">
                 <div className="flex flex-col w-max max-w-[320px] leading-1.5 p-4 border-zinc-200 bg-zinc-100 rounded-s-xl rounded-b-xl dark:bg-zinc-200">
                     <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <span className="text-sm font-semibold text-zinc-900 dark:text-black">{data.customer.name}</span>
+                        <span className="text-sm font-semibold text-zinc-900 dark:text-black">You</span>
                         <span className="text-sm font-normal text-zinc-500 dark:text-zinc-700">{data.humanized_created_at}</span>
                     </div>
                     <p className="text-sm font-normal break-words py-2.5 text-zinc-900 dark:text-black">{data.message}</p>
